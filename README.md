072c90ea07c3a0da5cb74498c915804312b577e4
Eric Zhang <eric@oc0757080034.ibm.com>
2014-04-22 17:39:41 +0800

    complete a full translation.

--- README.md
+++ README.md
@@ -3,8 +3,22 @@ CPAssistant
 
 Cantonese pronunciation assistant.
 ### Usage
+```
+python cp_assist.py [-h] [-v] [-f FILE] [-s STR]
 
+Convert Chinese to Cantonese.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -v, --version         show program's version number and exit
+  -f FILE, --file FILE  file path
+  -s STR, --str STR     chinese string
+```
 
 ### Dependencies
 * [jieba](https://github.com/fxsjy/jieba): Chinese text segmentation.
 * [CantoneseDict](https://github.com/Ho1iarty/CantoneseDict): A tool for creating a Cantonese dictionary(jyutping).
+
+
+### Update
+* 2014-04-22(Tue): Start project. Add file **lyrics** for test. To be continued.
--- cp_assist.py
+++ cp_assist.py
@@ -4,48 +4,89 @@
 # @Author  : Ho1iarty (ho1iarty@gmail.com)
 # @Version : 0.1
 
+import os
 import jieba
 from argparse import ArgumentParser
 import sqlite3 as lite
+import logging
+
+logging.basicConfig(filename = os.path.join(os.getcwd(), 'cpassistant.log'),
+    level = logging.DEBUG, filemode = 'a', 
+    format = '%(asctime)s - %(levelname)s: %(message)s')
+
+def contains_chinese(check_str):
+    for ch in check_str:
+        if u'\u4e00' <= ch <= u'\u9fff':
+            return True
+        return False
+
+def output(zi_list, yin_list):
+    out_zi_list = []
+    out_yin_list = []
+    output_list = []
+    for (idx,yin) in enumerate(yin_list):
+        if contains_chinese(zi_list[idx]):
+            for (idx_y,each_y) in enumerate(yin.split()):
+                out_yin_list.append('['+each_y+']')
+                out_zi_list.append(' '*(len(each_y)/2)+zi_list[idx][idx_y]+' '*(len(each_y)-len(each_y)/2))
+        else:
+            out_yin_list.append(yin)
+            out_zi_list.append(zi_list[idx])
+            if '\n' in yin:
+                output_list.append(''.join(out_yin_list))
+                output_list.append(''.join(out_zi_list))
+                del out_zi_list[:]
+                del out_yin_list[:]
+    # end for
+    if len(out_zi_list):
+        out_yin_list.append('\n')
+        out_zi_list.append('\n')
+        output_list.append(''.join(out_yin_list))
+        output_list.append(''.join(out_zi_list))
+    print ''.join(output_list)
+
 
 def get_jyutping(cur,phrase):
-    # 1. Exact Match
+    # Exact Match
     cur.execute("select YIN from DICT_CI where JIAN='"+phrase+"';")
     rst = cur.fetchone()
     if rst:
         return rst[0]
     else:
-        # 2. Fuzzy Match
-        cur.execute("select YIN from DICT_CI where JIAN like '%"+phrase+"%';")
-        rst = cur.fetchone()
-        if rst:
-            return rst[0]
-        else:
-            # 3. Matching separately
-            if len(phrase)==1:
-                cur.execute("select YIN from DICT_ZI where ZI='"+phrase+"';")
-                rst = cur.fetchone()
-                if rst:
-                    return rst[0].split()[0]
-                else:
-                    return '  '
+        # Matching separately
+        logging.info("No result in DICT_CI: %s" % phrase)
+        if len(phrase)==1:
+            cur.execute("select YIN from DICT_ZI where ZI='"+phrase+"';")
+            rst = cur.fetchone()
+            if rst:
+                return rst[0].split()[0]
             else:
-                tmp = []
-                for word in phrase:
-                    tmp.append(get_jyutping(cur, word))
-                return ' '.join(tmp)
+                return '  '
+        else:
+            tmp = []
+            for word in phrase:
+                tmp.append(get_jyutping(cur, word))
+            return ' '.join(tmp)
 
 def mark_sentence(sentence):
-    result = list(jieba.cut(sentence))
-    # result = [u'这是',u'一个',u'伸手不见五指',u'里',u'黑夜']
     # get db connection
     con = None
     try:
+        result = list(jieba.cut(sentence))
         con = lite.connect('jyut_zh.dict')
         cur = con.cursor()
+        zi_list = []
+        yin_list = []
         for phrase in result:
-            jyutp = get_jyutping(cur, phrase)
-            print phrase.encode('utf-8'), jyutp.encode('utf-8')
+            zi_list.append(phrase)
+            if contains_chinese(phrase):
+                jyutp = get_jyutping(cur, phrase)
+                yin_list.append(jyutp)
+                # print phrase.encode('utf-8'), jyutp.encode('utf-8')
+            else:
+                yin_list.append(phrase)
+        # end for
+        output(zi_list,yin_list)
     except Exception, e:
         raise e
     finally:
@@ -80,10 +121,9 @@ def main():
             # use string
             mark_sentence(options.str.decode('utf-8'))
         else:
-            print "Error!"
+            parser.print_help()
 
 if __name__ == "__main__":
-    mark_sentence("归家谁有几里路谁能预算")
-    # main()
+    main()
     
 
