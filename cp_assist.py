c41e0014dbf648ac033ca943fce9cb6a0fec1538
slandx <slandx.zx@gmail.com>
2022-10-17 23:10:07 +0800

    feat: support output pdf file

--- cp_assist.py
+++ cp_assist.py
@@ -4,12 +4,21 @@
 # @Author  : Ho1iarty (ho1iarty@gmail.com)
 # @Version : 0.1
 
+from operator import le
 import os
 import jieba
 from argparse import ArgumentParser
 import sqlite3 as lite
 import logging
 import chardet
+
+from reportlab.platypus import SimpleDocTemplate, Paragraph
+from reportlab.lib.styles import ParagraphStyle
+from reportlab.lib.units import inch
+from reportlab.rl_config import defaultPageSize
+from reportlab.pdfbase import pdfmetrics
+from reportlab.pdfbase.ttfonts import TTFont
+pdfmetrics.registerFont(TTFont('youyuan', 'youyuan.ttf'))
 
 jieba.setLogLevel(logging.INFO)
 
@@ -25,7 +34,29 @@ def contains_chinese(check_str):
         return False
 
 
-def output(zi_list, yin_list):
+def generate_pdf_file(pdf_name, lines):
+    doc = SimpleDocTemplate(
+        pdf_name,
+        pagesize=defaultPageSize,
+        bottomMargin=.4 * inch,
+        topMargin=.4 * inch,
+        rightMargin=.8 * inch,
+        leftMargin=.8 * inch)
+    style = ParagraphStyle(
+        name='Normal',
+        fontName='youyuan',
+        fontSize=9,
+    )
+    story = []
+    for value in lines:
+        P = Paragraph(value, style)
+        story.append(P)
+
+    doc.build(
+        story,
+    )
+
+def output(zi_list, yin_list, pdf_file_name):
     out_zi_list = []
     out_yin_list = []
     output_list = []
@@ -48,7 +79,10 @@ def output(zi_list, yin_list):
         out_zi_list.append('\n')
         output_list.append(''.join(out_yin_list))
         output_list.append(''.join(out_zi_list))
-    print(''.join(output_list))
+    if len(pdf_file_name):
+        generate_pdf_file(pdf_file_name, [x.replace(" ","&nbsp;") for x in output_list])
+    else:
+        print(''.join(output_list))
 
 
 def get_jyutping(cur, phrase):
@@ -74,7 +108,7 @@ def get_jyutping(cur, phrase):
             return ' '.join(tmp)
 
 
-def mark_sentence(sentence):
+def mark_sentence(sentence, pdf_file_name):
     # get db connection
     con = None
     try:
@@ -92,7 +126,7 @@ def mark_sentence(sentence):
             else:
                 yin_list.append(phrase)
         # end for
-        output(zi_list, yin_list)
+        output(zi_list, yin_list, pdf_file_name)
     except Exception as e:
         raise e
     finally:
@@ -106,6 +140,7 @@ def get_parser():
                         version='{0} {1}'.format('CP Assistant', '0.1 Beta'))
     parser.add_argument('-f', '--file', help='file path', type=str)
     parser.add_argument('-s', '--str', help='chinese string', type=str)
+    parser.add_argument('-p', '--pdf', help='output to PDF file', type=str)
     return parser
 
 
@@ -120,14 +155,14 @@ def main():
                 fileObj = open(options.file, 'rb')
                 f_content = fileObj.read()
                 enc_rst = chardet.detect(f_content)
-                mark_sentence(f_content.decode(enc_rst['encoding']))
+                mark_sentence(f_content.decode(enc_rst['encoding']), options.pdf)
             except Exception as e:
                 print(e)
             finally:
                 if fileObj:
                     fileObj.close()
         elif options.str:
-            mark_sentence(options.str)
+            mark_sentence(options.str, options.pdf)
         else:
             parser.print_help()
 
